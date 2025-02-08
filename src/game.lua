9182c8c29cc597cfb30484a16fea4984233214a3
Arwen Terpstra <ArwenTerpstra@proton.me>
2025-02-08 21:11:59 +0100

    Simple player setup

---
+++ src/game.lua
@@ -0,0 +1,10 @@
+function love.load()
+    player = { x = 400, y = 550, width = 32, height = 32 }
+end
+
+function love.update()
+end
+
+function love.draw()
+    love.graphics.rectangle("fill", player.x, player.y, player.width, player.height)
+end
